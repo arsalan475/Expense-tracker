e78bc1426291e467371ac3267c0763e75cdc7eb0
Arsalan <arsalan475@gmail.com>
2024-08-13 15:07:31 +0500

    add url

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -18,7 +18,7 @@ export default function Register() {
      
      try {
       
-    const res = await axios.post('https://expensetracker.onwebapp.io//register',{
+    const res = await axios.post('https://expensetracker.onwebapp.io/register',{
          userName,
          email,
          password
